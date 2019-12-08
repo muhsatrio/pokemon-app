a9cf6bd4ae873f81b14e74aae30368f06d7340cb
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-12-09 06:27:03 +0700

    updated PokemonDetail component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import {Provider} from 'react-redux';
 import { createStore } from 'redux';
 import NavbarPage from './components/NavbarPage';
--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import {Row, Col, Button} from 'react-bootstrap';
 
 const PokemonDetail = ({pokemon}) => {
 
+    const divStyle = {
+        padding: '10px',
+        textAlign: 'left',
+        maxWidth: '350px',
+        margin: '0 auto'
+    }
+
     return (
-    <div className="PokemonDetail">
-        <img src={pokemon.img} style={{height: '200px'}} alt="" /><br />
-        <span><b>Name</b>: {pokemon.name}</span><br />
-        <span><b>Moves</b>: {pokemon.move}</span><br />
-        <span><b>Types</b>: {pokemon.type}</span><br />
+    <div className="PokemonDetail" style={divStyle}>
+        <div style={{textAlign: 'center'}}>
+            <img src={pokemon.img} style={{height: '200px'}} alt="" />
+        </div>
+        <Row>
+            <Col xs="2"><b>Name: </b> </Col>
+            <Col xs="10">{pokemon.name}</Col>
+        </Row>
+        <Row>
+            <Col xs="2"><b>Moves: </b></Col>
+            <Col xs="10">{pokemon.move}</Col>
+        </Row>
+        <Row>
+            <Col xs="2"><b>Types: </b></Col>
+            <Col xs="10">{pokemon.type}</Col>
+        </Row>
+        <div style={{textAlign: 'center'}}>
+            <Button style={{margin: '15px'}} variant="success">CATCH!</Button>
+        </div>
     </div>
 )}
 
